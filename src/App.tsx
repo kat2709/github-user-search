17eb16b40682f017cb5685231e1bcd0d6ad1a870
Katsiaryna Rubchanka <6923845@mail.ru>
2024-05-04 12:15:33 +0300

    feat: add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,48 @@
+import MoonIcon from "./assets/moon.svg";
+// import SunIcon from "./assets/sun.svg";
+
 export function App() {
   return (
     <>
-      <h1>Hello world!</h1>
-      <Button>Hi Wo</Button>
-      <Button2>Hi Wo</Button2>
+      <Header></Header>
     </>
   );
 }
 
-function Button({ children }: any) {
+function Header({ children }: any) {
   return (
-    <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
+    <header className="w-full max-w-screen-lg h-10 flex items-center justify-between m-auto text-white uppercase">
       {children}
-    </button>
+      <div className="">User Name</div>
+      <div className="w-28 flex gap-2.5 justify-center items-center">
+        <div>Light</div>
+        <MoonIcon className="w-5 h-5 flex" />
+      </div>
+    </header>
   );
 }
-function Button2({ children }: any) {
-  return (
-    <button className="p-2 rounded bg-red-500 hover:bg-red-600 transition">
-      {children}
-    </button>
-  );
-}
+
+// export function App() {
+//   return (
+//     <>
+//       <h1>Hello world!</h1>
+//       <Button>Hi Wo</Button>
+//       <Button2>Hi Wo</Button2>
+//     </>
+//   );
+// }
+
+// function Button({ children }: any) {
+//   return (
+//     <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
+//       {children}
+//     </button>
+//   );
+// }
+// function Button2({ children }: any) {
+//   return (
+//     <button className="p-2 rounded bg-red-500 hover:bg-red-600 transition">
+//       {children}
+//     </button>
+//   );
+// }
