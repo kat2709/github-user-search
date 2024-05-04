74d763628e8c95b575cf20953b0be952a7c5fcc3
Katsiaryna Rubchanka <6923845@mail.ru>
2024-05-04 13:43:10 +0300

    feat: add search user box

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,51 @@
 import MoonIcon from "./assets/moon.svg";
+import SearchIcon from "./assets/search.svg";
 // import SunIcon from "./assets/sun.svg";
 
 export function App() {
   return (
-    <>
+    <main className="w-full max-w-screen-md m-auto">
       <Header></Header>
-    </>
+      <SearchBox></SearchBox>
+      <DevInfoBox></DevInfoBox>
+    </main>
   );
 }
 
 function Header({ children }: any) {
   return (
-    <header className="w-full max-w-screen-lg h-10 flex items-center justify-between m-auto text-white uppercase">
+    <header className="p-2.5 h-10 flex items-center justify-between text-white uppercase">
       {children}
-      <div className="">User Name</div>
-      <div className="w-28 flex gap-2.5 justify-center items-center">
+      <div className="">devfinder</div>
+      <div className="flex gap-2.5 justify-center items-center">
         <div>Light</div>
-        <MoonIcon className="w-5 h-5 flex" />
+        <MoonIcon className="w-5 h-5" />
       </div>
     </header>
   );
 }
 
-// export function App() {
-//   return (
-//     <>
-//       <h1>Hello world!</h1>
-//       <Button>Hi Wo</Button>
-//       <Button2>Hi Wo</Button2>
-//     </>
-//   );
-// }
+function SearchBox({ children }: any) {
+  return (
+    <div className="p-2.5 rounded-lg bg-violet-950 mt-5 flex gap-2.5 items-center">
+      {children}
+      <SearchIcon className="text-sky-500 bg-violet-950 w-8 h-8" />
+      <input
+        placeholder="Search GitHub username..."
+        className="w-full bg-violet-950 text-white p-1 border-none outline-none"
+      />
+      <button className="bg-sky-500 py-1.5 px-2.5 rounded-lg text-white">
+        Search
+      </button>
+    </div>
+  );
+}
 
-// function Button({ children }: any) {
-//   return (
-//     <button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
-//       {children}
-//     </button>
-//   );
-// }
-// function Button2({ children }: any) {
-//   return (
-//     <button className="p-2 rounded bg-red-500 hover:bg-red-600 transition">
-//       {children}
-//     </button>
-//   );
-// }
+function DevInfoBox({ children }: any){
+return (
+<div className="">
+{/* <UserAvatar></UserAvatar> */}
+<div></div>
+</div>
+)
+}
